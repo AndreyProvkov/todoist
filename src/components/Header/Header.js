e6b9ecffeec3c3b0cd1918c68331df37e14cc2ab
Andrey Provkov <provkovasu15@gmail.com>
2022-07-08 18:51:00 +0500

    Add display header logic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 
 export default class Header extends Component {
   render() {
+    let isActive = this.props.active;
+    let handle = undefined;
+
+    if (!isActive) {
+      handle = this.props.onEdit;
+    }
+
     return (
       <header className='header'>
         <h1 className='header__title'>Сегодня</h1>
-        <button className='btn-edit header__btn-edit'>Править</button>
+        {
+          this.props.tasks.length === 0 ? '' :
+            <button className='btn-edit header__btn-edit' onClick={handle}>
+            {this.props.edit ? 'Отменить' : 'Править'}
+          </button>
+        }
       </header>
     )
   }
