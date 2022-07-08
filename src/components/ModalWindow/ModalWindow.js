8b55df46c17f11e3262343ba7d0b069c034e1a41
Andrey Provkov <provkovasu15@gmail.com>
2022-07-08 18:50:12 +0500

    Add modal window display logic

--- src/components/ModalWindow/ModalWindow.js
+++ src/components/ModalWindow/ModalWindow.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
 
 export default class ModalWindow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+  
   render() {
+    let isEdit = this.props.edit;
+    let isActive = this.props.active;
+    let btn;
+
+    if (isActive && !isEdit) {
+      btn = (
+        <button className='btn-add modal__btn-add' onClick={() => this.props.addTask(this.state.value)}>Добавить</button>
+      );
+    }
+    if (isActive && isEdit) {
+      btn = (
+        <button className='btn-save modal__btn-save'>Сохранить</button>
+      )
+    }
+
     return (
-      <div>
-        <textarea>Enter</textarea>
-        <button>Закрыть</button>
-        <button>Добавить</button>
-        <button>Сохранить</button>
+      <div className='modal'>
+        <textarea className='modal__input' value={this.state.value} onChange={this.handleChange}>Enter</textarea>
+        <button className='btn-close modal__btn-close' onClick={this.props.closeModal}>Закрыть</button>
+        {btn}
       </div>
     )
   }
